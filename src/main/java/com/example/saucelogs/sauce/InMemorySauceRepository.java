f9cabb0156f56323388aae5cad30e48d1307cf12
joshgrant <joshua.m.grant@gmail.com>
2022-07-07 15:39:02 -0400

    just the service

--- src/main/java/com/example/saucelogs/sauce/InMemorySauceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.saucelogs.sauce;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InMemorySauceRepository extends CrudRepository<Sauce, Long> {}
